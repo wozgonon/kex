92adcfe37ae3e1e9072233fca2c57d783b0fd243
jb <wogonon@gmail.com>
2018-06-20 19:35:45 +0100

    Functioning

--- src/main/javascript/FileServer.js
+++ src/main/javascript/FileServer.js
@@ -10,17 +10,26 @@ let url  = require('url')
 
 let server = http.createServer (function (req, res) {
     let parsed_url = url.parse (req.url,true)
-    let pathname = parsed.pathname
-    let filePath = dir + .pathname
+    let pathname = parsed_url.pathname
+    let filePath = dir + pathname
+    if (! fs.existsSync (filePath)) {
+	res.writeHead (404, {'ContentType': 'text/html'})
+	res.write ("Not found: " + pathname)
+	res.end()
+	return
+    }
+
     let stat = fs.lstatSync (filePath)
     if (stat.isDirectory (filePath)) {
-	fs.readdir(filePath, err, files) => {
+	fs.readdir(filePath, (err, files) => {
 	    if (err === null) {
 		res.writeHead (200, {'ContentType': 'text/html'})
 		res.write ("<html><body>")
-		files.foreach (file => {
+		console.log(files)
+		files.forEach (file => {
+		    console.log (file)
 		    let fileName = file.toString ()
-		    let prefix = pathname == "/" ? "" : pathname
+		    let prefix = pathname == "/" ? "" : pathname + "/"
 		    let path = prefix + fileName
 		    res.write ("<li><a href=\"" + path + "\">" + fileName + "</a>")
 		})
@@ -30,7 +39,7 @@ let server = http.createServer (function (req, res) {
 		res.write (err.toString ())
 	    }
 	    res.end()
-	}
+	})
     } else { // if (stat.isFile (filePath)) {
 	fs.readFile (filePath, function (err, data) {
 	    if (err === null) {
